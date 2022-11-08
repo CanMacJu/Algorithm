1d8e2bbe6e51986d72d902dd2f6d40358c593230
CanMacJu <kimbaksa0220@gmail.com>
2022-11-08 16:59:23 +0900

    다익스트라 알고리즘

--- Algorithm/Algorithm.cpp
+++ Algorithm/Algorithm.cpp
@@ -4,6 +4,8 @@
 #include <list>
 #include <stack>
 #include <queue>
+#include <algorithm>
+#include <map>
 
 using namespace std;
 
@@ -19,11 +21,13 @@ struct Vertex
 vector<Vertex> vertices;
 vector<vector<int>> adjacent;
 vector<bool> discovered;
+vector<bool> visited;
+vector<int> cost;
 
 void CreateGraph()
 {
 	vertices.resize(6);
-	adjacent = vector<vector<int>>(6);
+	adjacent = vector<vector<int>>(6, vector<int>(6, -1));
 
 	// 인접 리스트
 	/*adjacent[0].push_back(1);
@@ -41,13 +45,23 @@ void CreateGraph()
 	// 인접 행렬
 	adjacent = vector<vector<int>>
 	{
-		{ 0, 1, 0, 1, 0, 0 },
-		{ 1, 0, 1, 1, 0, 0 },
-		{ 0, 0, 0, 0, 0, 0 },
-		{ 0, 0, 0, 0, 1, 0 },
-		{ 0, 0, 0, 0, 0, 0 },
-		{ 0, 0, 0, 0, 1, 0 }
+		{ -1, 15, -1, 35, -1, -1 },
+		{ 15, -1, +5, 10, -1, -1 },
+		{ -1, -1, -1, -1, -1, -1 },
+		{ -1, -1, -1, -1, +5, -1 },
+		{ -1, -1, -1, -1, -1, -1 },
+		{ -1, -1, -1, -1, +5, -1 }
 	};
+
+	/*adjacent = vector<vector<int>>
+	{
+		{ -1, 35, -1, 15, -1, -1 },
+		{ -1, -1, -1, -1, +7, -1 },
+		{ -1, -1, -1, -1, -1, -1 },
+		{ -1, 10, +8, -1, -1, -1 },
+		{ -1, -1, -1, -1, -1, -1 },
+		{ -1, -1, -1, -1, +5, -1 },
+	};*/
 }
 
 // DFS
@@ -207,13 +221,162 @@ void BfsAll()
 
 }
 
+
+// 다익스트라 알고리즘
+void Dijkstra(int here)
+{
+	vector<int> parent(6, -1);
+
+	queue<int> q;
+	q.push(here);
+	parent[here] = here;
+	cost[here] = 0;
+
+	while (q.empty() == false)
+	{
+		here = q.front();
+		q.pop();
+		visited[here] = true;
+
+		map<int, int> m;
+		m.clear();
+		for (int there = 0; there < adjacent[here].size(); ++there)
+		{
+			if (adjacent[here][there] == -1)
+			{
+				continue;
+			}
+
+			if (visited[there])
+			{
+				continue;
+			}
+
+			m.insert({ adjacent[here][there], there });
+
+			parent[there] = here;
+			int newCost = cost[here] + adjacent[here][there];
+			if (newCost < cost[there])
+			{
+				cost[there] = newCost;
+			}
+		}
+
+		for (auto iter = m.begin(); iter != m.end(); ++iter)
+		{
+			q.push(iter->second);
+		}
+
+		/*for (auto iter : m)
+		{
+			int Index = iter.second;
+			q.push(Index);
+		}*/
+		
+	}
+
+	for (int i = 0; i < cost.size(); ++i)
+	{
+		if (cost[i] == INT_MAX)
+		{
+			continue;
+		}
+
+		cout << "Index: " << i << endl;
+		cout << "Cost: " << cost[i] << endl;
+		cout << endl;
+	}
+}
+
+void Dijkstra2(int here)
+{
+	struct VertexCost
+	{
+		int vertex;
+		int cost;
+	};
+
+	// 발견 목록
+	list<VertexCost> discovered;
+	vector<int> best(6, INT32_MAX);
+	vector<int> parent(6, -1);
+
+	discovered.push_back(VertexCost{here, 0});
+	best[here] = 0;
+	parent[here] = 0;
+
+	while (discovered.empty() == false)
+	{
+		// 제일 좋은 후보를 찾음
+		list<VertexCost>::iterator bestIt;
+		int bestCost = INT32_MAX;
+
+		for (auto iter = discovered.begin(); iter != discovered.end(); ++iter)
+		{
+			const int cost = iter->cost;
+
+			if (cost < bestCost)
+			{
+				bestCost = cost;
+				bestIt = iter;
+			}
+		}
+
+		int cost = bestIt->cost;
+		here = bestIt->vertex;
+		discovered.erase(bestIt);
+
+
+		if (best[here] < cost)
+		{
+			continue;
+		}
+
+		for (int there = 0; there < 6; there++)
+		{
+			if (adjacent[here][there] == -1)
+			{
+				continue;
+			}
+
+			int nextCost = best[here] + adjacent[here][there];
+			if (best[there] < nextCost)
+			{
+				continue;
+			}
+
+			discovered.push_back(VertexCost{ there, nextCost });
+			best[there] = nextCost;
+			parent[there] = here;
+		}
+
+	}
+
+	for (int i = 0; i < best.size(); ++i)
+	{
+		if (best[i] == INT32_MAX)
+		{
+			continue;
+		}
+
+		cout << "Vertex: " << i << endl;
+		cout << "  Cost: " << best[i] << endl;
+		cout << "Parent: " << parent[i] << endl;
+		cout << endl;
+	}
+
+}
+
+
 int main()
 {
 	CreateGraph();
 
 	discovered = vector<bool>(6, false);
+	visited = vector<bool>(6, false);
+	cost = vector<int>(6, INT_MAX);
 
-	BfsAll();
+	Dijkstra2(0);
 
 	return 0;
 }
