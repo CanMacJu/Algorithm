931eb04f9ac42cd0253036b8d26a5bc20c442ed9
CanMacJu <kimbaksa0220@gmail.com>
2022-11-15 18:16:13 +0900

    이진 탐색

--- Algorithm/Algorithm.cpp
+++ Algorithm/Algorithm.cpp
@@ -6,213 +6,54 @@
 #include <queue>
 #include <algorithm>
 #include <map>
+#include "BinarySearchTree.h"
 
 using namespace std;
 
-using NodeRef = shared_ptr<struct Node>;
+// 이진 탐색 트리
 
-struct Node
+
+
+// 이진 탐색 (binary search)
+// 
+
+vector<int> numbers;
+
+void BinarySearch(int N)
 {
-	Node() {}
-	Node(const string& data) : data(data) {}
+	int right = (int)numbers.size() - 1;
+	int left = 0;
 
-	string data;
+	int cost = 1;
 
-	vector<NodeRef> children;
-};
-
-template<typename T, typename Container = vector<T>, typename Predicate = less<T>>
-class priority_Queue
-{
-public:
-
-	void push(const T& data)
+	while (left <= right)
 	{
-		int now = static_cast<int>(_heap.size());
-		_heap.push_back(data);
+		int mid = (left + right) / 2;
 
-		while (now != 0)
+		if (N < numbers[mid])
 		{
-			int parent = (now - 1) / 2;
-			if (_predicate(_heap[now], _heap[parent]))
-			{
-				break;
-			}
-
-			std::swap(_heap[parent], _heap[now]);
-			now = parent;
+			right = mid - 1;
+			cost++;
+		}
+		else if (N > numbers[mid])
+		{
+			left = mid + 1;
+			cost++;
+		}
+		else
+		{
+			cout << cost << "번 만에 찾음" << endl;
+			return;
 		}
 	}
 
-	void pop()
-	{
-		_heap[0] = _heap.back();
-		_heap.pop_back();
-
-		int now = 0;
-		while (true)
-		{
-			int next = now;
-
-			int left = (now * 2) + 1;
-			if (left >= _heap.size())
-			{
-				break;
-			}
-
-			// 왼쪽 값과 비교
-			if (_predicate(_heap[now], _heap[left]))
-			{
-				next = left;
-			}
-
-			// 위 비교 중 큰 수와 오른쪽 비교
-			int right = (now * 2) + 2;
-			if (right < _heap.size() && _predicate(_heap[next], _heap[right]))
-			{
-				next = right;
-			}
-
-			if (now == next)
-			{
-				break;
-			}
-
-			std::swap(_heap[next], _heap[now]);
-			now = next;
-		}
-	}
-
-
-	T& top()
-	{
-		return _heap[0];
-	}
-
-	bool empty()
-	{
-		return _heap.empty();
-	}
-
-	int size()
-	{
-		return static_cast<int>(_heap.size());
-	}
-
-private:
-	Container _heap = {};
-	Predicate _predicate = {};
-
-};
-
-NodeRef CreateTree()
-{
-	NodeRef root = make_shared<Node>("R1 개발실");
-	{
-		NodeRef node = make_shared<Node>("디자인팀");
-		root->children.push_back(node);
-		{
-			NodeRef leaf = make_shared<Node>("전투");
-			node->children.push_back(leaf);
-		}
-		{
-			NodeRef leaf = make_shared<Node>("경제");
-			node->children.push_back(leaf);
-		}
-		{
-			NodeRef leaf = make_shared<Node>("스토리");
-			node->children.push_back(leaf);
-		}
-	}
-	{
-		NodeRef node = make_shared<Node>("프로그래밍팀");
-		root->children.push_back(node);
-		{
-			NodeRef leaf = make_shared<Node>("서버");
-			node->children.push_back(leaf);
-		}
-		{
-			NodeRef leaf = make_shared<Node>("클라");
-			node->children.push_back(leaf);
-		}
-		{
-			NodeRef leaf = make_shared<Node>("엔진");
-			node->children.push_back(leaf);
-		}
-	}
-	{
-		NodeRef node = make_shared<Node>("아트팀");
-		root->children.push_back(node);
-		{
-			NodeRef leaf = make_shared<Node>("배경");
-			node->children.push_back(leaf);
-		}
-		{
-			NodeRef leaf = make_shared<Node>("캐릭터");
-			node->children.push_back(leaf);
-		}
-	}
-
-	return root;
+	cout << "존재 하지 않음" << endl;
 }
-
-void PrintTree(NodeRef root, int depth)
-{
-	for (int i = 0; i < depth; ++i)
-	{
-		cout << "--";
-	}
-	cout << root->data << endl;
-
-	for (auto child : root->children)
-	{
-		PrintTree(child, depth + 1);
-	}
-}
-
-int GetHeight(NodeRef root)
-{
-	int Height = 1;
-	for (auto child : root->children)
-	{
-		if (Height < GetHeight(child) + 1)
-		{
-			Height = GetHeight(child) + 1;
-		}
-
-		//Height = max(Height, GetHeight(child) + 1);
-	}
-
-	return Height;
-}
-
-
-
 
 int main()
 {
-	auto pQ = priority_Queue<int, vector<int>, less<int>>();
-	pQ.push(3);
-	pQ.push(5);
-	pQ.push(10);
-	pQ.push(50);
-	pQ.push(2);
-	pQ.push(100);
-	pQ.push(90);
-	pQ.push(11);
-	pQ.push(1);
-	pQ.push(15);
-
-
-	cout << "Size: " << pQ.size() << endl;
-
-	while (pQ.empty() == false)
-	{
-		int value = pQ.top();
-		pQ.pop();
-
-		cout << "Value: " << value << endl;
-	}
+	numbers = vector<int>{ 1, 8, 15, 23, 32, 44, 56, 63, 81, 91, 100, 102, 103, 105, 106, 109};
+	BinarySearch(1);
 
 	return 0;
 }
