d48a890dbd1adfcdc1c22058d3e08557cae47a8d
CanMacJu <kimbaksa0220@gmail.com>
2022-12-09 17:43:47 +0900

    kruskal 알고리즘

--- Algorithm/Algorithm.cpp
+++ Algorithm/Algorithm.cpp
@@ -12,18 +12,10 @@
 using namespace std;
 
 
-// 그래프/트리 응용
-// 푀소 스패닝 트리
-
-// 상호 베타적 집합 (Disjoint Set)
-// -> 유니온-파인드
-
-
-// 시간복잡도: 트리의 높이에 비례
-class NaiveDisjointSet
+class DisjointSet
 {
 public:
-	NaiveDisjointSet(int n) : _parent(n)
+	DisjointSet(int n) : _parent(n), _rank(n, 1)
 	{
 		for (int i = 0; i < n; ++i)
 		{
@@ -33,62 +25,6 @@ public:
 
 	int Find(int u)
 	{
-		while (_parent[u] != u)
-		{
-			u = _parent[u];
-		}
-
-		return u;
-
-
-
-		/*if (u == _parent[u])
-		{
-			return u;
-		}
-
-		return Find(_parent[u]);*/
-	}
-
-	void Merge(int u, int v)
-	{
-		u = Find(u);
-		v = Find(v);
-
-		if (u == v)
-			return;
-
-		_parent[u] = v;
-	}
-
-private:
-	vector<int> _parent;
-
-};
-
-// 트리가 한쪽으로 기우는 문제를 해결
-// -> 트리를 합칠때, 높이가 낮은 트리를 높이가 높은 트리에 붙임
-// -> Union-By-Rank
-class DisjointSet1
-{
-public:
-	DisjointSet1(int n) : _parent(n), _rank(n, 1)
-	{
-		for (int i = 0; i < n; ++i)
-		{
-			_parent[i] = i;
-		}
-	}
-
-	int Find(int u)
-	{
-		/*while (_parent[u] != u)
-		{
-			u = _parent[u];
-		}
-
-		return u;*/
-
 		if (u == _parent[u])
 		{
 			return u;
@@ -118,85 +54,103 @@ public:
 		}
 	}
 
-
 private:
 	vector<int> _parent;
 	vector<int> _rank;
 
 };
 
-class DisjointSet2
+struct Vertex
 {
-public:
-	DisjointSet2(int n) : _parent(n), _childs(n)
-	{
-		for (int i = 0; i < n; ++i)
-		{
-			_parent[i] = i;
-		}
-	}
-
-	int Find(int u)
-	{
-		while (_parent[u] != u)
-		{
-			u = _parent[u];
-		}
-
-		return u;
-
-		/*if (u == _parent[u])
-		{
-			return u;
-		}
-
-		return Find(_parent[u]);*/
-	}
-
-	void Merge(int u, int v)
-	{
-		u = Find(u);
-		v = Find(v);
-
-		if (u == v)
-			return;
-
-		if (Height(u) <= Height(v))
-		{
-			_parent[u] = v;
-			_childs[v].push_back(u);
-		}
-		else
-		{
-			_parent[v] = u;
-			_childs[u].push_back(v);
-		}
-	}
-
-	int Height(int u)
-	{
-		int maxHeight = 0;
-		for (int n : _childs[u])
-		{
-			if (maxHeight < Height(n))
-			{
-				maxHeight = Height(n);
-			}
-		}
-
-		return maxHeight + 1;
-	}
-
-private:
-	vector<int> _parent;
-	vector<vector<int>> _childs;
-
+	// int data;
 };
+
+vector<Vertex> vertices;
+vector<vector<int>> adjacent;
+
+void CreateGraph()
+{
+	vertices.resize(6);
+	adjacent = vector<vector<int>>(6, vector<int>(6, -1));
+
+	adjacent[0][1] = adjacent[1][0] = 15;
+	adjacent[0][3] = adjacent[3][0] = 35;
+	adjacent[1][2] = adjacent[2][1] = 5;
+	adjacent[1][3] = adjacent[3][1] = 10;
+	adjacent[3][4] = adjacent[4][3] = 5;
+	adjacent[3][5] = adjacent[5][3] = 10;
+	adjacent[4][5] = adjacent[5][4] = 5;
+}
+
+
+struct CostEdge
+{
+	int cost;
+	int u;
+	int v;
+
+	bool operator<(CostEdge& other)
+	{
+		return cost < other.cost;
+	}
+};
+
+int Kruskal(vector<CostEdge>& selected)
+{
+	int ret = 0;
+
+	selected.clear();
+
+	vector<CostEdge> edges;
+
+	for (int u = 0; u < adjacent.size(); ++u)
+	{
+		for (int v = 0; v < adjacent[u].size(); ++v)
+		{
+			if (u > v)
+			{
+				continue;
+			}
+
+			int cost = adjacent[u][v];
+
+			if (cost == -1)
+			{
+				continue;
+			}
+
+			edges.push_back(CostEdge{ cost, u, v });
+		}
+	}
+
+	std::sort(edges.begin(), edges.end());
+
+	DisjointSet sets(vertices.size());
+
+	for (CostEdge& edge : edges)
+	{
+		if (sets.Find(edge.u) == sets.Find(edge.v))
+		{
+			continue;
+		}
+
+		sets.Merge(edge.u, edge.v);
+		selected.push_back(edge);
+		ret += edge.cost;
+	}
+
+	return ret;
+}
+
 
 
 int main()
 {
+	CreateGraph();
 
+	vector<CostEdge> selected;
+
+	int cost = Kruskal(selected);
 
 
 	return 0;
